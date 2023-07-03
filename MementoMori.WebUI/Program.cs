49f1c4c4531f7244a55916d15de4aa1e4ddddd20
moonheart <themoonheart@outlook.com>
2023-07-04 00:20:14 +0800

    use blazorize

--- MementoMori.WebUI/Program.cs
+++ MementoMori.WebUI/Program.cs
@@ -1,33 +1,47 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using MementoMori;
 using MementoMori.WebUI.Data;
+using MementoMori.WebUI.ViewModels;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddJsonFile("appsettings.dev.json", true, true);
+        builder.Configuration.AddJsonFile("appsettings.dev.json", true, true);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddSingleton<MementoMoriFuncs>();
+        builder.Services.AddBlazorise(opt => { })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
 
-builder.Services.AddOptions();
-builder.Services.Configure<AuthOption>(builder.Configuration.GetSection("AuthOption"));
-builder.Services.Configure<GameConfig>(builder.Configuration.GetSection("GameConfig"));
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<MementoMoriFuncs>();
+        builder.Services.AddSingleton<CharactorUnitViewModel>();
 
-var app = builder.Build();
+        builder.Services.AddOptions();
+        builder.Services.Configure<AuthOption>(builder.Configuration.GetSection("AuthOption"));
+        builder.Services.Configure<GameConfig>(builder.Configuration.GetSection("GameConfig"));
+
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapBlazorHub();
+        app.MapFallbackToPage("/_Host");
+
+        app.Run();
+    }
 }
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
---
+++ MementoMori.WebUI/ViewModels/CharactorUnitViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Reactive.Linq;
+using ReactiveUI;
+
+namespace MementoMori.WebUI.ViewModels;
+
+public class CharactorUnitViewModel : ReactiveObject
+{
+    private int _count;
+
+    public int Count
+    {
+        get => _count;
+        set => this.RaiseAndSetIfChanged(ref _count, value);
+    }
+
+    public CharactorUnitViewModel()
+    {
+        Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1)).Subscribe(l =>
+        {
+            Count++;
+        });
+    }
+}
