71e5173bc85d52cce43a838b1260c0a7b67f204e
moonheart <themoonheart@outlook.com>
2023-09-24 01:49:28 +0800

    add asset downloader

---
+++ MementoMori.AssetDownloader/AssetDownloader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MementoMori;
+
+namespace ConsoleApp1;
+
+internal class AssetDownloader : BackgroundService
+{
+    private readonly MementoNetworkManager _networkManager;
+    private const string GameOs = "Android";
+
+    public AssetDownloader(MementoNetworkManager networkManager)
+    {
+        _networkManager = networkManager;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await _networkManager.DownloadAssets(stoppingToken);
+            await Task.Delay(TimeSpan.FromMinutes(60), stoppingToken);
+        }
+    }
+}
---
+++ MementoMori.AssetDownloader/Program.cs
@@ -0,0 +1,26 @@
+﻿using MementoMori;
+using MessagePack;
+using MementoMori.Exceptions;
+using MementoMori.Ortega.Share.Data.ApiInterface;
+using MementoMori.Ortega.Share.Data;
+using MementoMori.Ortega.Share;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Reflection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            builder.Services.AddSingleton<MementoNetworkManager>();
+            builder.Services.AddHostedService<AssetDownloader>();
+
+            IHost host = builder.Build();
+            host.Run();
+        }
+    }
+}
