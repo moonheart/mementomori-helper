b256f78e51f24f63b79351a28fd8809b7c9ebe21
moonheart <themoonheart@outlook.com>
2023-10-07 12:46:01 +0800

    add docker build script

---
+++ build_docker_downloader.sh
@@ -0,0 +1,2 @@
+docker build -t moonheartmoon/mementomori-downloader:v1 -f MementoMori.AssetDownloader/Dockerfile .
+docker push moonheartmoon/mementomori-downloader:v1
---
+++ build_docker_webui.sh
@@ -0,0 +1,2 @@
+docker build -t moonheartmoon/mementomori-webui:v1 -f MementoMori.WebUI/Dockerfile .
+docker push moonheartmoon/mementomori-webui:v1
