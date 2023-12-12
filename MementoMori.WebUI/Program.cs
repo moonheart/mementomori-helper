c162d50104985eda7d13172c3217d6b70056058b
moonheart <themoonheart@outlook.com>
2023-12-12 22:49:22 +0800

    upgrade to net8.0

--- MementoMori.WebUI/Program.cs
+++ MementoMori.WebUI/Program.cs
@@ -42,11 +42,12 @@ internal class Program
 
         builder.Services.AddMudServices();
 
-        // builder.Services.AddRazorComponents()
-        //     .AddInteractiveServerComponents();
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
 
-        builder.Services.AddRazorPages();
-        builder.Services.AddServerSideBlazor();
+        //builder.Services.AddRazorPages();
+        //builder.Services.AddServerSideBlazor();
+
         builder.Services.AddSingleton<AtlasManager>();
         builder.Services.AddSingleton<AccountManager>();
         builder.Services.AddTransient<MementoNetworkManager>();
@@ -87,15 +88,14 @@ internal class Program
         if (!app.Environment.IsDevelopment()) app.UseExceptionHandler("/Error");
 
         app.UseStaticFiles();
-        // app.UseAntiforgery();
-        // app.MapRazorComponents<App>()
-        //     .AddInteractiveServerRenderMode();
-
+        app.UseAntiforgery();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
         app.UseSentryTracing();
-        app.UseRouting();
+        //app.UseRouting();
 
-        app.MapBlazorHub();
-        app.MapFallbackToPage("/_Host");
+        //app.MapBlazorHub();
+        //app.MapFallbackToPage("/_Host");
 
         app.Services.GetRequiredService<AccountManager>().AutoLogin().ConfigureAwait(false).GetAwaiter().GetResult();
 
