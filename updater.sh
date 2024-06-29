b9a08fa53f0be13fda962e8e5d09151d4e10aedd
少姜 <80018096+PackageInstaller@users.noreply.github.com>
2024-06-29 23:40:38 +0800

    support more languages ​​and architectures (#87)
    * support more languages ​​and architectures
    
    * lite change

--- updater.sh
+++ updater.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# 1.请将脚本放在解压后的文件夹内，它的树状图应该是这样：
-#
 # publish-linux-x64/
 # ├── appsettings.json
 # ├── MementoMori.Common.pdb
@@ -11,27 +9,47 @@
 # ├── MementoMori.WebUI.pdb
 # ├── updater.sh
 # └── wwwroot
-#
-# 2.确保你的系统中已经安装了 jq wget unzip 这三个工具
-#
-# 3.接着赋予这个脚本可执行权限
-#
-# 4.最后，在终端中执行它
 
 # 设置GitHub仓库信息
 repository="moonheart/mementomori-helper"
 releaseApiUrl="https://api.github.com/repos/$repository/releases/latest"
 downloadUrl="https://github.com/$repository/releases/download"
 
+# 获取语言环境
+lang=$(locale | grep LANG | cut -d= -f2 | cut -d. -f1)
+
+# ANSI转义码颜色
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+NC="\033[0m"
+
 # 检查所需工具的可用性
 required_tools=("jq" "wget" "unzip")
+missing_tools=()
 
 for tool in "${required_tools[@]}"; do
     if ! command -v "$tool" &> /dev/null; then
-        echo "错误：请安装 $tool 等必要的包."
-        exit 1
+        missing_tools+=("$tool")
     fi
 done
+
+if [ ${#missing_tools[@]} -ne 0 ]; then
+    case "$lang" in
+        zh_CN|zh_SG)
+            echo -e "${RED}错误：请安装以下必要的包: ${missing_tools[*]}${NC}"
+            ;;
+        zh_TW|zh_HK)
+            echo -e "${RED}錯誤：請安裝以下必要的包: ${missing_tools[*]}${NC}"
+            ;;
+        ja_JP)
+            echo -e "${RED}エラー：以下の必要なパッケージをインストールしてください: ${missing_tools[*]}${NC}"
+            ;;
+        *)
+            echo -e "${RED}Error: Please install the necessary packages: ${missing_tools[*]}${NC}"
+            ;;
+    esac
+    exit 1
+fi
 
 # 指定程序文件名和下载目录
 programFileName="MementoMori.WebUI"
@@ -49,6 +67,37 @@ if [ -d "$lastTempDir" ]; then
     rm -rf "$lastTempDir"
 fi
 
+# 获取系统架构
+arch=$(uname -m)
+case "$arch" in
+    x86_64)
+        zipName="publish-linux-x64.zip"
+        ;;
+    armv7l)
+        zipName="publish-linux-arm.zip"
+        ;;
+    aarch64)
+        zipName="publish-linux-arm64.zip"
+        ;;
+    *)
+        case "$lang" in
+            zh_CN|zh_SG)
+                echo -e "${RED}错误：不支持的系统架构 $arch.${NC}"
+                ;;
+            zh_TW|zh_HK)
+                echo -e "${RED}錯誤：不支持的系統架構 $arch.${NC}"
+                ;;
+            ja_JP)
+                echo -e "${RED}エラー：サポートされていないシステムアーキテクチャ $arch。${NC}"
+                ;;
+            *)
+                echo -e "${RED}Error: Unsupported system architecture $arch.${NC}"
+                ;;
+        esac
+        exit 1
+        ;;
+esac
+
 # 获取最新的发布信息
 latestRelease=$(curl -s "$releaseApiUrl")
 latestVersionStr=$(echo "$latestRelease" | jq -r '.tag_name')
@@ -56,7 +105,20 @@ latestVersion=$(echo "$latestVersionStr" | sed 's/^v//')
 
 # 检查是否成功获取最新版本信息
 if [ -z "$latestRelease" ] || [ "$latestVersionStr" = "null" ]; then
-    echo "错误：无法获取最新版本,请检查网络连接是否正常."
+    case "$lang" in
+        zh_CN|zh_SG)
+            echo -e "${RED}错误：无法获取最新版本,请检查网络连接是否正常.${NC}"
+            ;;
+        zh_TW|zh_HK)
+            echo -e "${RED}錯誤：無法獲取最新版本,請檢視網路連接狀態.${NC}"
+            ;;
+        ja_JP)
+            echo -e "${RED}エラー：最新バージョンを取得できません。ネットワーク接続を確認してください。${NC}"
+            ;;
+        *)
+            echo -e "${RED}Error: Unable to retrieve the latest version, please check your network connection.${NC}"
+            ;;
+    esac
     exit 1
 fi
 
@@ -64,11 +126,38 @@ fi
 currentVersion=$(strings "$downloadDirectory/$programFileName" | grep -o 'MementoMori.WebUI/[0-9]\+\.[0-9]\+\.[0-9]\+' | awk '!seen[$0]++' | sed 's/MementoMori.WebUI\///')
 
 if [ "$(printf '%s\n' "$currentVersion" "$latestVersion" | sort -V | head -n 1)" = "$latestVersion" ]; then
-    echo "程序已经是最新版本 ($latestVersion). 无需更新."
+    case "$lang" in
+        zh_CN|zh_SG)
+            echo -e "${GREEN}程序已经是最新版本 ($latestVersion). 无需更新.${NC}"
+            ;;
+        zh_TW|zh_HK)
+            echo -e "${GREEN}程式已經是最新版本 ($latestVersion). 無需更新.${NC}"
+            ;;
+        ja_JP)
+            echo -e "${GREEN}プログラムはすでに最新バージョンです ($latestVersion)。更新は不要です。${NC}"
+            ;;
+        *)
+            echo -e "${GREEN}The program is already up-to-date ($latestVersion). No update is necessary.${NC}"
+            ;;
+    esac
 else
-
+    case "$lang" in
+        zh_CN|zh_SG)
+            echo -e "${GREEN}系统架构为: $arch，开始更新...${NC}"
+            ;;
+        zh_TW|zh_HK)
+            echo -e "${GREEN}系統架構為: $arch，開始更新...${NC}"
+            ;;
+        ja_JP)
+            echo -e "${GREEN}システムアーキテクチャ: $arch、更新を開始します...${NC}"
+            ;;
+        *)
+            echo -e "${GREEN}System architecture: $arch, starting update...${NC}"
+            ;;
+    esac
+    
     # 下载最新版本的压缩包
-    downloadLink="$downloadUrl/$latestVersionStr/publish-linux-x64.zip"
+    downloadLink="$downloadUrl/$latestVersionStr/$zipName"
     downloadPath="$downloadDirectory/latest.zip"
     wget -O "$downloadPath" "$downloadLink"
 
@@ -81,10 +170,33 @@ else
     pkill -f "$programFileName" || true
 
     # 复制解压后的文件到程序文件夹
-    cp -Rf "$tempDir/publish-linux-x64/"* "$downloadDirectory/"
+    case "$arch" in
+        x86_64)
+            cp -Rf "$tempDir/publish-linux-x64/"* "$downloadDirectory/"
+            ;;
+        armv7l)
+            cp -Rf "$tempDir/publish-linux-arm/"* "$downloadDirectory/"
+            ;;
+        aarch64)
+            cp -Rf "$tempDir/publish-linux-arm64/"* "$downloadDirectory/"
+            ;;
+    esac
 
     # 删去临时文件夹与下载的包
     rm -rf "$tempDir" "$downloadPath"
 
-    echo "程序已成功更新到版本 $latestVersion."
+    case "$lang" in
+        zh_CN|zh_SG)
+            echo -e "${GREEN}程序已成功更新到 $latestVersion.${NC}"
+            ;;
+        zh_TW|zh_HK)
+            echo -e "${GREEN}程式已成功更新到 $latestVersion.${NC}"
+            ;;
+        ja_JP)
+            echo -e "${GREEN}プログラムはバージョン $latestVersion に正常に更新されました。${NC}"
+            ;;
+        *)
+            echo -e "${GREEN}The program has been successfully updated to version $latestVersion.${NC}"
+            ;;
+    esac
 fi
