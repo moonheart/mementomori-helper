7393df1672f7e63fc8e08178397fa31d66facdca
moonheart <themoonheart@outlook.com>
2024-07-12 23:05:41 +0800

    use items that are full before receive

--- MementoMori/Funcs/Present.cs
+++ MementoMori/Funcs/Present.cs
@@ -1,83 +1,80 @@
-﻿using MementoMori.Common.Localization;
-using MementoMori.Exceptions;
-using MementoMori.Extensions;
+﻿using MementoMori.Exceptions;
 using MementoMori.Ortega.Custom;
-using MementoMori.Ortega.Share;
 using MementoMori.Ortega.Share.Data.ApiInterface.Equipment;
 using MementoMori.Ortega.Share.Data.ApiInterface.Item;
 using MementoMori.Ortega.Share.Data.ApiInterface.Present;
 using MementoMori.Ortega.Share.Data.Item.Model;
-using MementoMori.Ortega.Share.Enums;
 
 namespace MementoMori;
 
 public partial class MementoMoriFuncs
 {
+    private const double UseOverLimitItemRate = 0.3;
+
     public async Task PresentReceiveItem()
     {
         await ExecuteQuickAction(async (log, token) =>
         {
-            var usedItem = false;
             do
             {
                 var getListResponse = await GetResponse<GetListRequest, GetListResponse>(new GetListRequest {LanguageType = LanguageType.zhTW});
                 if (getListResponse.userPresentDtoInfos.Any(d => !d.IsReceived))
                 {
+                    // use over limit items before receiving
+                    // only QuestQuickTicket and Equipment are considered
+                    foreach (var presentItem in getListResponse.userPresentDtoInfos.SelectMany(d => d.ItemList).GroupBy(d => new {d.Item.ItemType, d.Item.ItemId}))
+                    {
+                        if (presentItem.Key.ItemType == ItemType.QuestQuickTicket)
+                        {
+                            var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                            var itemMb = ItemTable.GetByItemTypeAndItemId(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                            var maxItemCount = itemMb.MaxItemCount;
+                            if (count < maxItemCount) continue;
+
+                            var name = TextResourceTable.Get(itemMb.NameKey);
+                            var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
+                            log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
+                            var response = await GetResponse<UseAutoBattleRewardItemRequest, UseAutoBattleRewardItemResponse>(new UseAutoBattleRewardItemRequest
+                            {
+                                ItemType = (QuestQuickTicketType) itemMb.ItemId,
+                                UseCount = useCount
+                            });
+                            response.RewardItemList.PrintUserItems(log);
+                        }
+                        else if (presentItem.Key.ItemType == ItemType.Equipment)
+                        {
+                            var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                            var maxItemCount = 999;
+                            if (count < maxItemCount) continue;
+
+                            var name = TextResourceTable.Get(EquipmentTable.GetById(presentItem.Key.ItemId).NameKey);
+                            var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
+                            log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
+                            var response = await GetResponse<CastRequest, CastResponse>(new CastRequest {UserEquipment = new UserEquipment(presentItem.Key.ItemId, useCount)});
+                            response.ResultItemList.PrintUserItems(log);
+                        }
+                    }
+
                     try
                     {
                         var resp = await GetResponse<ReceiveItemRequest, ReceiveItemResponse>(new ReceiveItemRequest {LanguageType = LanguageType.zhTW});
-                        usedItem = false;
-                        log($"{Masters.TextResourceTable.Get("[MyPagePresentBoxButtonTitle]")} {Masters.TextResourceTable.Get("[MyPagePresentBoxButtonAllReceive]")}");
+                        log($"{TextResourceTable.Get("[MyPagePresentBoxButtonTitle]")} {TextResourceTable.Get("[MyPagePresentBoxButtonAllReceive]")}");
                         resp.ResultItems.Select(d => d.Item).PrintUserItems(log);
                     }
                     catch (ApiErrorException e) when (e.ErrorCode == ErrorCode.PresentReceiveOverLimitCountPresent)
                     {
                         log(e.Message);
+                        // if there are no QuestQuickTicket or Equipment, break the loop
                         var grp = getListResponse.userPresentDtoInfos.SelectMany(d => d.ItemList).GroupBy(d => new {d.Item.ItemType, d.Item.ItemId});
-                        if (grp.Count(d => d.Key.ItemType != ItemType.ExchangePlaceItem) == 0) break;
-
-                        foreach (var presentItem in grp)
-                        {
-                            if (presentItem.Key.ItemType == ItemType.QuestQuickTicket)
-                            {
-                                var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                                var itemMb = Masters.ItemTable.GetByItemTypeAndItemId(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                                var maxItemCount = itemMb.MaxItemCount;
-                                if (count < maxItemCount) continue;
-
-                                var name = Masters.TextResourceTable.Get(itemMb.NameKey);
-                                var useCount = (int) Math.Floor(maxItemCount * 0.3);
-                                log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
-                                var response = await GetResponse<UseAutoBattleRewardItemRequest, UseAutoBattleRewardItemResponse>(new UseAutoBattleRewardItemRequest
-                                {
-                                    ItemType = (QuestQuickTicketType) itemMb.ItemId,
-                                    UseCount = useCount
-                                });
-                                response.RewardItemList.PrintUserItems(log);
-                                usedItem = true;
-                                break;
-                            }
-
-                            if (presentItem.Key.ItemType == ItemType.Equipment)
-                            {
-                                var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                                var maxItemCount = 999;
-                                if (count < maxItemCount) continue;
-
-                                var name = Masters.TextResourceTable.Get(Masters.EquipmentTable.GetById(presentItem.Key.ItemId).NameKey);
-                                var useCount = (int) Math.Floor(maxItemCount * 0.3);
-                                log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
-                                var response = await GetResponse<CastRequest, CastResponse>(new CastRequest {UserEquipment = new UserEquipment(presentItem.Key.ItemId, useCount)});
-                                response.ResultItemList.PrintUserItems(log);
-                                usedItem = true;
-                                break;
-                            }
-                        }
+                        if (grp.Count(d => d.Key.ItemType == ItemType.QuestQuickTicket || d.Key.ItemType == ItemType.Equipment) == 0) break;
                     }
                 }
                 else
-                    log(Masters.TextResourceTable.GetErrorCodeMessage(ErrorCode.PresentReceiveAlreadyReceivedPresent));
-            } while (usedItem);
+                {
+                    log(TextResourceTable.GetErrorCodeMessage(ErrorCode.PresentReceiveAlreadyReceivedPresent));
+                    break;
+                }
+            } while (true);
         });
     }
 }
