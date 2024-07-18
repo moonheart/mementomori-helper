67969ab61bce5c54fb1c534b8e046a9d37aa25ef
moonheart <themoonheart@outlook.com>
2024-07-18 23:46:31 +0800

    fix receive present loop

--- MementoMori.Ortega/Share/Data/ApiInterface/Present/ReceiveItemRequest.cs
+++ MementoMori.Ortega/Share/Data/ApiInterface/Present/ReceiveItemRequest.cs
@@ -4,24 +4,12 @@ using MessagePack;
 
 namespace MementoMori.Ortega.Share.Data.ApiInterface.Present
 {
-	[MessagePackObject(true)]
-	[OrtegaApi("present/receiveItem", true, false)]
-	public class ReceiveItemRequest : ApiRequestBase
-	{
-		public LanguageType LanguageType
-		{
-			get;
-			set;
-		}
+    [MessagePackObject(true)]
+    [OrtegaApi("present/receiveItem", true, false)]
+    public class ReceiveItemRequest : ApiRequestBase
+    {
+        public LanguageType LanguageType { get; set; }
 
-		public string PresentGuid
-		{
-			get;
-			set;
-		}
-
-		public ReceiveItemRequest()
-		{
-		}
-	}
+        public string PresentGuid { get; set; }
+    }
 }
--- MementoMori/Funcs/Present.cs
+++ MementoMori/Funcs/Present.cs
@@ -15,66 +15,73 @@ public partial class MementoMoriFuncs
     {
         await ExecuteQuickAction(async (log, token) =>
         {
-            do
+            var getListResponse = await GetResponse<GetListRequest, GetListResponse>(new GetListRequest {LanguageType = LanguageType.zhTW});
+            if (getListResponse.userPresentDtoInfos.Any(d => !d.IsReceived))
             {
-                var getListResponse = await GetResponse<GetListRequest, GetListResponse>(new GetListRequest {LanguageType = LanguageType.zhTW});
-                if (getListResponse.userPresentDtoInfos.Any(d => !d.IsReceived))
+                // use over limit items before receiving
+                // only QuestQuickTicket and Equipment are considered
+                foreach (var presentItem in getListResponse.userPresentDtoInfos.SelectMany(d => d.ItemList).GroupBy(d => new {d.Item.ItemType, d.Item.ItemId}))
                 {
-                    // use over limit items before receiving
-                    // only QuestQuickTicket and Equipment are considered
-                    foreach (var presentItem in getListResponse.userPresentDtoInfos.SelectMany(d => d.ItemList).GroupBy(d => new {d.Item.ItemType, d.Item.ItemId}))
+                    if (presentItem.Key.ItemType == ItemType.QuestQuickTicket)
                     {
-                        if (presentItem.Key.ItemType == ItemType.QuestQuickTicket)
+                        var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                        var itemMb = ItemTable.GetByItemTypeAndItemId(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                        var maxItemCount = itemMb.MaxItemCount;
+                        if (count < maxItemCount) continue;
+
+                        var name = TextResourceTable.Get(itemMb.NameKey);
+                        var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
+                        log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
+                        var response = await GetResponse<UseAutoBattleRewardItemRequest, UseAutoBattleRewardItemResponse>(new UseAutoBattleRewardItemRequest
                         {
-                            var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                            var itemMb = ItemTable.GetByItemTypeAndItemId(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                            var maxItemCount = itemMb.MaxItemCount;
-                            if (count < maxItemCount) continue;
-
-                            var name = TextResourceTable.Get(itemMb.NameKey);
-                            var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
-                            log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
-                            var response = await GetResponse<UseAutoBattleRewardItemRequest, UseAutoBattleRewardItemResponse>(new UseAutoBattleRewardItemRequest
-                            {
-                                ItemType = (QuestQuickTicketType) itemMb.ItemId,
-                                UseCount = useCount
-                            });
-                            response.RewardItemList.PrintUserItems(log);
-                        }
-                        else if (presentItem.Key.ItemType == ItemType.Equipment)
-                        {
-                            var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
-                            var maxItemCount = 999;
-                            if (count < maxItemCount) continue;
-
-                            var name = TextResourceTable.Get(EquipmentTable.GetById(presentItem.Key.ItemId).NameKey);
-                            var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
-                            log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
-                            var response = await GetResponse<CastRequest, CastResponse>(new CastRequest {UserEquipment = new UserEquipment(presentItem.Key.ItemId, useCount)});
-                            response.ResultItemList.PrintUserItems(log);
-                        }
+                            ItemType = (QuestQuickTicketType) itemMb.ItemId,
+                            UseCount = useCount
+                        });
+                        response.RewardItemList.PrintUserItems(log);
                     }
+                    else if (presentItem.Key.ItemType == ItemType.Equipment)
+                    {
+                        var count = UserSyncData.GetUserItemCount(presentItem.Key.ItemType, presentItem.Key.ItemId);
+                        var maxItemCount = 999;
+                        if (count < maxItemCount) continue;
 
-                    try
-                    {
-                        var resp = await GetResponse<ReceiveItemRequest, ReceiveItemResponse>(new ReceiveItemRequest {LanguageType = LanguageType.zhTW});
-                        log($"{TextResourceTable.Get("[MyPagePresentBoxButtonTitle]")} {TextResourceTable.Get("[MyPagePresentBoxButtonAllReceive]")}");
-                        resp.ResultItems.Select(d => d.Item).PrintUserItems(log);
-                    }
-                    catch (ApiErrorException e) when (e.ErrorCode == ErrorCode.PresentReceiveOverLimitCountPresent)
-                    {
-                        log(e.Message);
-                        // if there are no QuestQuickTicket or Equipment, break the loop
-                        var grp = getListResponse.userPresentDtoInfos.SelectMany(d => d.ItemList).GroupBy(d => new {d.Item.ItemType, d.Item.ItemId});
-                        if (grp.Count(d => d.Key.ItemType == ItemType.QuestQuickTicket || d.Key.ItemType == ItemType.Equipment) == 0) break;
+                        var name = TextResourceTable.Get(EquipmentTable.GetById(presentItem.Key.ItemId).NameKey);
+                        var useCount = (int) Math.Floor(maxItemCount * UseOverLimitItemRate);
+                        log($"{ResourceStrings.UseOverLimitItem}: {name}×{useCount}, {count}/{maxItemCount}");
+                        var response = await GetResponse<CastRequest, CastResponse>(new CastRequest {UserEquipment = new UserEquipment(presentItem.Key.ItemId, useCount)});
+                        response.ResultItemList.PrintUserItems(log);
                     }
                 }
-                else
+
+                try
                 {
-                    log(TextResourceTable.GetErrorCodeMessage(ErrorCode.PresentReceiveAlreadyReceivedPresent));
-                    break;
+                    var resp = await GetResponse<ReceiveItemRequest, ReceiveItemResponse>(new ReceiveItemRequest {LanguageType = NetworkManager.LanguageType});
+                    log($"{TextResourceTable.Get("[MyPagePresentBoxButtonTitle]")} {TextResourceTable.Get("[MyPagePresentBoxButtonAllReceive]")}");
+                    resp.ResultItems.Select(d => d.Item).PrintUserItems(log);
                 }
-            } while (true);
+                catch (ApiErrorException e) when (e.ErrorCode == ErrorCode.PresentReceiveOverLimitCountPresent)
+                {
+                    log(e.Message);
+                    // receive one by one
+                    foreach (var present in getListResponse.userPresentDtoInfos.Where(d => !d.IsReceived))
+                    {
+                        try
+                        {
+                            var resp = await GetResponse<ReceiveItemRequest, ReceiveItemResponse>(
+                                new ReceiveItemRequest {LanguageType = NetworkManager.LanguageType, PresentGuid = present.Guid});
+                            resp.ResultItems.Select(d => d.Item).PrintUserItems(log);
+                        }
+                        catch (ApiErrorException e2) when (e2.ErrorCode == ErrorCode.PresentReceiveOverLimitCountPresent)
+                        {
+                            // ignored
+                        }
+                    }
+                }
+            }
+            else
+            {
+                log(TextResourceTable.GetErrorCodeMessage(ErrorCode.PresentReceiveAlreadyReceivedPresent));
+            }
         });
     }
 }
