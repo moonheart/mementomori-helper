2f4a9cb4c084e77787c1a4c3c9d7acbcf15ac171
moonheart <themoonheart@outlook.com>
2024-10-09 23:12:50 +0800

    add notice interface

---
+++ MementoMori.Ortega/Share/Data/ApiInterface/Notice/GetMyPageNoticeInfoListRequest.cs
@@ -0,0 +1,14 @@
+﻿using MementoMori.Ortega.Share.Enums;
+using MessagePack;
+
+namespace MementoMori.Ortega.Share.Data.ApiInterface.Notice
+{
+    [MessagePackObject(true)]
+    [OrtegaApi("notice/getMypageNoticeInfoList", true, false)]
+    public class GetMyPageNoticeInfoListRequest : ApiRequestBase
+    {
+        public NoticeCategoryType CategoryType { get; set; }
+
+        public LanguageType LanguageType { get; set; }
+    }
+}
---
+++ MementoMori.Ortega/Share/Data/ApiInterface/Notice/GetMyPageNoticeInfoListResponse.cs
@@ -0,0 +1,11 @@
+﻿using MementoMori.Ortega.Share.Data.Notice;
+using MessagePack;
+
+namespace MementoMori.Ortega.Share.Data.ApiInterface.Notice
+{
+    [MessagePackObject(true)]
+    public class GetMyPageNoticeInfoListResponse : ApiResponseBase
+    {
+        public List<NoticeInfo> NoticeInfoList { get; set; }
+    }
+}
