879ace5a0b318108015b58d86b58143e414f50ce
moonheart <themoonheart@outlook.com>
2026-02-21 19:28:04 +0800

    feat: Add API request and response models for deleting presents.

---
+++ MementoMori.Ortega/Share/Data/ApiInterface/Present/DeletePresentRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+using MessagePack;
+
+namespace MementoMori.Ortega.Share.Data.ApiInterface.Present
+{
+	[MessagePackObject(true)]
+	[OrtegaApi("present/deletePresent", true, false)]
+	public class DeletePresentRequest : ApiRequestBase
+	{
+        public List<string> PresentGuidList { get; set; }
+    }
+}
---
+++ MementoMori.Ortega/Share/Data/ApiInterface/Present/DeletePresentResponse.cs
@@ -0,0 +1,9 @@
+﻿using MessagePack;
+
+namespace MementoMori.Ortega.Share.Data.ApiInterface.Present
+{
+	[MessagePackObject(true)]
+	public class DeletePresentResponse : ApiResponseBase
+	{
+	}
+}
